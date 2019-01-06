db12ba4bf23e0ab4a90a8dbe52f7808834efb795
ParsaSharmila <44161437+ParsaSharmila@users.noreply.github.com>
2019-01-06 23:41:32 +0200

    Add files via upload

---
+++ Angular Tutorial/js/controllers/tutorialCtrl.js
@@ -0,0 +1,43 @@
+angular.module("tutorialCtrlModule", [])
+
+.controller("TutorialCtrl", ["$scope", "Calculations", function($scope, Calculations) {
+
+$scope.tutorialObject = {};
+$scope.tutorialObject.title = "Main Page";
+$scope.tutorialObject.subTitle ="Sub title";
+
+$scope.tutorialObject.bindOutput = 2;
+
+$scope.tutorialObject.firstname = "Thomas";
+$scope.tutorialObject.lastname ="Brown";
+
+$scope.timesTwo = function(){
+$scope.tutorialObject.bindOutput = Calculations.timesTwo($scope.tutorialObject.bindOutput);
+}
+
+}])
+
+.factory("Calculations", function(){
+	var calculations = {};
+	calculations.timesTwo = function(a){
+		return a * 2;
+	};
+	return calculations;
+		
+
+})
+
+.controller('TutorialCtrl2', ['$scope', function($scope){
+	$scope.secondTutorial = "This is second tutorial";
+
+	
+}]);
+
+
+
+/*.directive("tbWelcomeMessage", function(){
+	return {
+		restrict: "AE",
+		template: "<div>Howdy how are you?</div>"
+	}
+});*/
